78ba38558c4926b4c1938542520374f636ec6553
jose <josepthc9781@gmail.com>
2020-02-02 14:24:24 -0500

    Plantilla con visualización (temporal) de propiedades

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>Plantilla</title>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700,100' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
   </head>
@@ -13,20 +14,45 @@
 
             </ul>
             <ul class="nav navbar-nav navbar-center">
-                <li><a href="#" style="color: red; font-weight: bold; margin-right: 150px">CONSTRUYENDO SUEÑOS</a></li>
+                <li><a href="#" style="color: red; font-weight: bold; margin: 10px; font-size: 22px;">CONSTRUYENDO SUEÑOS</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right" style="margin-right:150px">
-                <li><a href="#" style="font-weight: bold"><img src="{{asset('img/usuario.png')}}" style="margin-right: 10px">Login</a></li>
+                <li><a href="#" style="font-weight: bold"><img src="{{asset('img/usuario.png')}}" style="margin-right: 10px; color: black">Login</a></li>
             </ul>
         </div>
         <!-- BARRA ROJA -->
         <div class="barra"></div>
     </nav>
 
-    <!-- IMAGEN -->
+    <!-- IMAGEN DEL LOGO-->
     <div class="imagen">
       <img src=" {{asset('img/logo.jpeg') }}" class="imagen_fixed" alt="">
     </div>
+
+    <!-- IMAGEN DE FONDO-->
+    <div class="imagen_central">
+      @yield('central')
+    </div>
+
+    <div class="container">
+      @yield('content')
+    </div>
+
+    <footer>
+      <div class="container">
+          <div class="row">
+              <div class="col-md-8">
+                  <h3>Your Logo</h3>
+                  <br>
+                  <h4>© 2016 Your Company. Designed and Developed by <a target="_blank" href="http://www.themeinthebox.com">ThemeintheBox.com</a></h4>
+              </div>
+              <div class="col-md-4">
+                  <p class="text-right social"><i class="typcn typcn-social-facebook-circular"></i><i class="typcn typcn-social-twitter-circular"></i><i class="typcn typcn-social-tumbler-circular"></i><i class="typcn typcn-social-github-circular"></i><i class="typcn typcn-social-dribbble-circular"></i></p>
+              </div>
+          </div>
+        </div>
+    </footer>
+
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,52 @@
 @extends('template')
+
+@section('central')
+  <img src="{{asset('img/conjunto.jpg')}}" alt="">
+@endsection
+
+@section('content')
+<div class="">
+  <div class="row" style="background-color:#f4f4f4; margin-top: 10px">
+    <div class="col-md-12">
+      <hr>
+        <h1 style="text-align: center"><i>Recorrido por las propiedades</i></h1>
+      <hr>
+    </div>
+  </div>
+
+  <div class="row fondo" style="margin-top: 10px; margin-bottom: 10px">
+    <div class="col-md-6">
+      <img src="{{asset('img/casa1.jpg')}}" alt="" class="imagen_setting">
+    </div>
+    <div class="col-md-6">
+      <h2 style="text-align: center"><i>Propiedad 1</i></h2>
+      <h3>Tipo:</h3>
+      <h3>Ciudad:</h3>
+      <h3>Barrio</h3>
+      <h3>Area</h3>
+      <h3>Tiene 5 habitaciones</h3>
+    </div>
+  </div>
+  <hr style="background-color: black">
+  <div class="row fondo2" style="margin-bottom: 10px">
+    <div class="col-md-6">
+      <img src="{{asset('img/casa1.jpg')}}" alt="" class="imagen_setting">
+    </div>
+    <div class="col-md-6">
+      <h2 style="text-align: center"><i>Propiedad 2</i></h2>
+      <h3>Tipo:</h3>
+      <h3>Ciudad:</h3>
+      <h3>Barrio</h3>
+      <h3>Area</h3>
+      <h3>Tiene 5 habitaciones</h3>
+    </div>
+  </div>
+  <hr style="background-color: black">
+
+</div>
+
+
+
+
+
+@endsection
